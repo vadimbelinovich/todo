417eaf6a00f12be5513d4506f325ffd187757106
Billie Willie <vadimbelinovich@gmail.com>
2019-04-01 06:19:19 +0300

    adds create function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const addButton = document.querySelector(".add");
+const input = document.querySelector(".input");
+const planed = document.querySelector(".planed");
+const done = document.querySelector(".done");
+
+function createEl() {
+  let item = document.createElement("li");
+
+  let checkbox = document.createElement("span");
+  checkbox.classList.add("check");
+  checkbox.innerHTML = "<i class='fas fa-square'></i>";
+
+  let text = document.createElement("label");
+  text.classList.add("text");
+
+  let input = document.createElement("input");
+
+  let deleteIcon = document.createElement("span");
+  deleteIcon.classList.add("delete");
+  deleteIcon.innerHTML = "<i class='fas fa-trash'></i>";
+
+  let editIcon = document.createElement("span");
+  editIcon.classList.add("edit");
+  editIcon.innerHTML = "<i class='fas fa-pen'></i>";
+
+  item.appendChild(checkbox);
+  item.appendChild(text);
+  item.appendChild(input);
+  item.appendChild(deleteIcon);
+  item.appendChild(editIcon);
+}
+
+console.log(done);
