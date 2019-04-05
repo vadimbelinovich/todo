bf39fa7aedba98a67d11dff2a6b2282000fda65a
Billie Willie <vadimbelinovich@gmail.com>
2019-04-06 02:43:42 +0300

    new one

--- js/app.js
+++ js/app.js
@@ -10,29 +10,64 @@ function createEl(task) {
 
   let checkbox = document.createElement("span");
   checkbox.classList.add("check");
-  checkbox.innerHTML = "<i class='fas fa-square'></i>";
+  checkbox.innerHTML = "<i class='far fa-square'></i>";
 
   let text = document.createElement("label");
   text.classList.add("text");
   text.innerHTML = task;
 
-  let input = document.createElement("input");
-
   let deleteIcon = document.createElement("span");
   deleteIcon.classList.add("delete");
   deleteIcon.innerHTML = "<i class='fas fa-trash'></i>";
 
-  let editIcon = document.createElement("span");
-  editIcon.classList.add("edit");
-  editIcon.innerHTML = "<i class='fas fa-pen'></i>";
-
   item.appendChild(checkbox);
   item.appendChild(text);
-  item.appendChild(input);
   item.appendChild(deleteIcon);
-  item.appendChild(editIcon);
 
   return item;
+}
+
+function addTask() {
+  if (input.value) {
+    let item = createEl(input.value);
+    planed.append(item);
+    bindTask(item, finishTask);
+    input.value = "";
+  }
+}
+
+addButton.addEventListener("click", addTask);
+
+function deleteTask() {
+  let li = this.parentNode;
+  let ul = li.parentNode;
+  ul.removeChild(li);
+}
+
+function finishTask() {
+  let li = this.parentNode;
+  let checkbox = li.querySelector(".check");
+  checkbox.innerHTML = "<i class='fas fa-check-square'></i>";
+
+  done.append(li);
+  bindTask(li, unfinishTask);
+}
+
+function unfinishTask() {
+  let li = this.parentNode;
+  let checkbox = li.querySelector(".check");
+  checkbox.innerHTML = "<i class='far fa-square'></i>";
+
+  planed.append(li);
+  bindTask(li, finishTask);
+}
+
+function bindTask(el, checkboxEvent) {
+  let checkEl = el.querySelector(".check");
+  let deleteEl = el.querySelector(".delete");
+
+  checkEl.onclick = checkboxEvent;
+  deleteEl.onclick = deleteTask;
 }
 
 console.log(createEl(1234));
